4814aacb6ffde89e3e0972b47f81f60703e6f87e
Dmytro Bukai <dbukai@kaaiot.io>
2020-04-12 08:53:43 +0300

    feat(command): added possibility to run specific command

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 import signal
 import sys
 
+from procedures.registry import ProcedureRegistry
 from sara.language import get_language, set_language
 from sara.sara import SaraController
 from sara.decorators import log
@@ -24,17 +25,33 @@ def signal_handler(cancelling):
 def parse_args():
     parser = argparse.ArgumentParser(description='Sara arguments')
     parser.add_argument('-ln', nargs='?', default='en', help='russian language')
+    parser.add_argument('-cmd', nargs='?', default='', help='procedure activator')
 
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = parse_args()
+def run_command(ln, command):
+    r = ProcedureRegistry(ln)
 
-    set_language(args.ln)
-    language = get_language()
-    sara = SaraController(language)
+    ps = r.detect_procedures(command)
+    for p in ps:
+        p.proceed(command)
+
+
+def run_sara(ln):
+    sara = SaraController(ln)
 
     signal.signal(signal.SIGINT, signal_handler(sara.cancelling))
     print('Press Ctrl+C')
     signal.pause()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    set_language(args.ln)
+    language = get_language()
+
+    if args.cmd:
+        run_command(language, args.cmd)
+    else:
+        run_sara(language)
